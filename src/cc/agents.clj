561027008a9bbe607716f71379cb661e8e847bff
Alex Collins <alextcollins@gmail.com>
2012-07-29 19:41:41 +0100

    Complete GUI with text area

--- src/cc/agents.clj
+++ src/cc/agents.clj
@@ -1,6 +1,6 @@
 (ns cc.agents
   (:import (java.awt BorderLayout Dimension))
-  (:import (javax.swing JLabel JFrame JList JScrollPane DefaultListModel)))
+  (:import (javax.swing JLabel JFrame JScrollPane JTextArea)))
 
 (def primes (agent []))
 
@@ -16,7 +16,7 @@
   "Find all primes up to i inclusive"
   (if (< i 2) 
 	[]
-	(let []
+	(do
 	  (dotimes [n (inc i)]
 		(if (prime? n) (send primes #(conj %1 n))))
 	  (await primes) ; block until primes is ready
@@ -27,20 +27,18 @@
   "Find all primes up to i inclusive and present them in a GUI"
   (let [fr (JFrame. "Prime Numbers")
         lbl (JLabel. (str "Here are all the prime numbers for " i ":"))
-        lm (DefaultListModel.)
-        jl (JList. lm)
-        sp (JScrollPane. jl)
+        ta (JTextArea.)
+        sp (JScrollPane. ta)
         pane (.getContentPane fr)]
     (def f (future (find-primes i))) ; asynchronously find the primes while we set up the GUI
     (.setPreferredSize lbl (Dimension. 410 20))
     (.setPreferredSize sp (Dimension. 410 190))
+    (.setLineWrap ta true)
     (.setSize fr 410 210)
-    (.setVisibleRowCount jl 5)
     (.add pane lbl BorderLayout/PAGE_START)
     (.add pane sp BorderLayout/CENTER)
+    (.pack fr)
     (.setVisible fr true)
-    ;(println (count @f))
-    (dotimes [n 10] (.addElement lm (str "Moo " n))
-    (println "Done"))))
+    (dotimes [n (count @f)] (.setText ta (str (.getText ta) (@f n) "\n")))))
 
 (show-primes 60000)
