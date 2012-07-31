1bc37a82f8305e6ce692debe1645cd1510ecae29
Alex Collins <alextcollins@gmail.com>
2012-07-31 21:47:54 +0100

    Add names and ages refs, write to all in dosync and then provide a (dotimes) for writing to the refs 100 x

--- src/cc/refs.clj
+++ src/cc/refs.clj
@@ -8,16 +8,20 @@
 
 (def names (ref ()))
 
-(defn rand-num [] (* 100 (rand)))
+(def counter (ref 0))
+
+(defn rint [] (rand-int (* (rand) 100)))
 
 (defn record-person [p]
   (dosync
-    (ref-set people (cons p @people)
+    (ref-set people (cons p @people))
     (ref-set ages (cons (p :age) @ages))
-    (ref-set names (cons (p :name) @ages))))
+    (ref-set names (cons (p :name) @names))
+    (ref-set counter (inc @counter))))
 
 (defn rand-person []
   "Create a new person struct with random name and age"
-  (struct person (str (char (rand-num))) (int (rand-num))))
+  (struct person (str (char (rint))) (rint)))
 
-(println (rand-person))
+(dotimes [n 100] (future (record-person (struct person (str "No. " n) n))))
+
